11f4dfa9855bd44f6c187a12c950e5404c7609e4
enzo0999 <enzogaldino@colegiopaulodetarso.com.br>
2025-06-23 16:21:14 +0000

    Aula 2.5

---
+++ Objetos/Objetos2.js
@@ -0,0 +1,12 @@
+const polvo = {
+    tinta: {
+    cor: preto,
+    },
+    raça: "Polvo do mar",
+    altura: "80cm",
+    pés: "8",
+    mudarCanal: function() {
+        console.log(cachorro.raça);
+    }
+};
+cachorro.mudarCanal();
