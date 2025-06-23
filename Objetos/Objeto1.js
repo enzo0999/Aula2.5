811c76bddfdd2f1916a77913fdf8cc89a9d87ef9
enzo0999 <enzogaldino@colegiopaulodetarso.com.br>
2025-06-23 16:00:59 +0000

    Aula 2.5

---
+++ Objetos/Objeto1.js
@@ -0,0 +1,10 @@
+const gato = {
+    animal: "gato",
+    peso:"80",
+    raça: "ragdoll",
+    cor: "branco",
+    mudarCanal: function() {
+        console.log("CanalAlterado");
+    }
+};
+gato.mudarCanal();
